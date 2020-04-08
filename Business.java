5039d36a58cfa5a1c3c8b30022203c83116c47e6
Nail Furkan Acar <furkan.acar@ozu.edu.tr>
2020-04-08 21:07:33 +0300

    Create Business.java

---
+++ Business.java
@@ -0,0 +1,13 @@
+package cs102project01;
+
+public class Business extends Faculties {
+	
+	public Business(String name) {
+		super(name);
+	}
+	
+	public String toString() {
+		return "Department Name: " + getName() + "\n"  + super.toString(); 
+	}
+}
+
