a021dc34402a7bdded9bba8e2fa5ba1c4858bd1e
Nail Furkan Acar <furkan.acar@ozu.edu.tr>
2020-04-09 11:25:24 +0300

    Set the scholarship for all faculties

--- Faculties.java
+++ Faculties.java
@@ -16,7 +16,19 @@ public class Faculties extends Department {
 			return "If you pass the Trace and finish " + getName()
 					+ " department in 4 years, you will have paid a total of " + (getCalcTotalPrice()) + " TL. \n"
 					+ "If you finish " + getName() + " department in 5 years, you will have paid a total of "
-					+ (getCalcTotalPrice() + getPREPPRICE()) + " TL.";
+					+ (getCalcTotalPrice() + getPREPPRICE()) + " TL." + "\n\n" + 
+					
+					"If you get 25% scholarship and If you pass the Trace and finish " + getName()
+					+ " department in 4 years, you will have paid a total of " + ((getCalcTotalPrice())*(3/4)) + " TL. \n"
+					+ "If you finish " + getName() + " department in 5 years, you will have paid a total of "
+					+ ((getCalcTotalPrice() + getPREPPRICE())*(3/4)) + " TL." + "\n\n" +
+					
+					"If you get 50% scholarship and If you pass the Trace and finish " + getName()
+					+ " department in 4 years, you will have paid a total of " + ((getCalcTotalPrice())*(1/2)) + " TL. \n"
+					+ "If you finish " + getName() + " department in 5 years, you will have paid a total of "
+					+ ((getCalcTotalPrice() + getPREPPRICE())*(1/2)) + " TL." + "\n\n" +
+					"If you get 100% scholarship, you will not pay the fees.";
+					
 
 		}
 	}
